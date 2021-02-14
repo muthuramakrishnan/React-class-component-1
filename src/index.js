2c824ef0667e8cbef4d246ab9fd499a8ea05d8a4
Muthuramakrishnan Mahadevan <muthu.rama.krishnan96@gmail.com>
2021-02-14 23:52:49 +0530

    class-component-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat:position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat)
+        {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat)
+        {
+            return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>
+        }
+        return <Loader message="Please accept location request"></Loader>
+    }
+
+    render(){
+        return (
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+        );
+        /*dont add conditionals inside render.. bcoz if we have a new requirement then we need to modify each and every condition.  Instead add a helper method*/
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
